cad4b27655414cb28c0aafe0a546af625e29ef74
Thiveeya <rbsthivi97@gmail.com>
2023-08-20 17:14:42 +0530

    deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_URI = 'http://localhost:8000'
+const API_URI = 'https://gmailclone-be.onrender.com';
 
 const API_GMAIL = async (serviceUrlObject, requestData = {}, type) => {
    // eslint-disable-next-lines
